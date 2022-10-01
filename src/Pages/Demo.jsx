1e7d9ae1ec71751e9435f09a8930a570eae03a14
Gedelachandrasekhar126396 <gedelachandrasekhar@gmail.com>
2022-10-01 18:45:39 +0530

    updated

--- src/Pages/Demo.jsx
+++ src/Pages/Demo.jsx
@@ -47,17 +47,56 @@ export default function DemoPage(){
             <p className = {styles.p1} >Watch this tutorial to learn how to keep track of your employees' work with the Admin features of DeskTime, find out how to add team members, manage work schedules, create invoices and reports, and more.</p>
             <p className = {styles.p1} >With DeskTime you can plan and create shifts, manage absences and use the Project tracking feature to see who is working on what and for how long.</p>
             <ul className= {styles.unlisted}>
-                    <h3>No software installation necessary</h3>
-                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i> Fully automated time tracker</li>
-                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i> App URL & Document title reacking</li>
-                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i> Project and task tracking</li>
+                    
+                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i> Keep track of processes and goals.</li>
+                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i> Set Automatic time & project tracking.</li>
+                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i> Grasp shift and absence Scheduling.</li>
                     </ul>
-            <button className= {styles.button1} >BOOK A DEMO</button>
-            <button className= {styles.DemoButton1} >START A FREE TRAIL</button>
+            <button className= {styles.button1} >START A FREE TRAIL</button>
+            <button className= {styles.DemoButton1} >BOOK A DEMO</button>
             
         </div>
         <div className= {styles.HomePage1a} >
-            <img src="https://desktime.com/static/web/demo/demo-of-desktime.png" alt="" />
+            <img id = {styles.Image2} src="https://desktime.com/static/web/demo/yt-dt-teams.png" alt="" />
+        </div>
+        </div>
+        <div className= {styles.DemoPage4} >
+        <div className= {styles.HomePage1a} >
+            <img id = {styles.Image2} src="https://desktime.com/static/web/demo/yt-dt-freelancers.png" alt="" />
+        </div>
+        <div>
+            <h1 className= {styles.TheUltimate}  style ={{textAlign: "left"}} >DeskTime for Teams</h1>
+            <p className = {styles.p1} >Watch this tutorial to learn how to keep track of your employees' work with the Admin features of DeskTime, find out how to add team members, manage work schedules, create invoices and reports, and more.</p>
+            <p className = {styles.p1} >With DeskTime you can plan and create shifts, manage absences and use the Project tracking feature to see who is working on what and for how long.</p>
+            <ul className= {styles.unlisted}>
+                    
+                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i> Identify your computer usage habits.</li>
+                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i> Improve your work plan & execution.</li>
+                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i>  See what's causing constraints in your day.</li>
+                    </ul>
+            <button className= {styles.button1} >START A FREE TRAIL</button>
+            <button className= {styles.DemoButton1} >BOOK A DEMO</button>
+            
+        </div>
+        
+        </div>
+        <div className= {styles.DemoPage4} >
+        <div>
+            <h1 className= {styles.TheUltimate}  style ={{textAlign: "left"}} >DeskTime for Freelancres</h1>
+            <p className = {styles.p1} >This guide explores the main features of DeskTime for freelancers and shows how to manage projects, create reports, and change the settings specific to you.</p>
+            <p className = {styles.p1} >Using DeskTime is a great way to never again get underpaid for the time invested in your clients' projects.</p>
+            <ul className= {styles.unlisted}>
+                    
+                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i> Track time spent on specific projects.</li>
+                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i> Use the Cost calculation and Invoicing features.</li>
+                 <li className = {styles.List} > <i id = {styles.DEF} class="fa-solid fa-check"></i> Boost productivity to raise your income.</li>
+                    </ul>
+            <button className= {styles.button1} >START A FREE TRAIL</button>
+            <button className= {styles.DemoButton1} >BOOK A DEMO</button>
+            
+        </div>
+        <div  style={{box:"none"}} className= {styles.HomePage1a} >
+            <img id = {styles.Image2} src="https://desktime.com/static/web/demo/yt-dt-teams.png" alt="" />
         </div>
         </div>
 
